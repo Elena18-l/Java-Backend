6dab1e547bb7e8819b4e7c64a2c402d98283d2d5
Elena <elopezestr@uoc.edu>
2025-03-24 18:29:27 +0100

    interefaces crud done

--- src/main/java/Repositorio/AlquilerRepositorio.java
+++ src/main/java/Repositorio/AlquilerRepositorio.java
@@ -1,6 +1,21 @@
 package Repositorio;
 import Modelo.Alquiler;
+import Modelo.Cliente;
+import Modelo.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface AlquilerRepositorio extends JpaRepository<Alquiler, Long> {
+    Alquiler save (Alquiler alquiler);
+    Optional <Alquiler> findById (Long id);
+    List<Alquiler> findAll();
+    void deleteById(Long id);
+    void delete (Alquiler alquiler);
+    long count();
+    List<Alquiler> FindByCliente (Cliente cliente);
+    List<Alquiler> FindByVehiculo(Vehiculo vehiculo);
+    List<Alquiler> FindEntreFechas(LocalDate fecha_inicio, LocalDate fecha_fin);
 }
--- src/main/java/Repositorio/VehiculoRepositorio.java
+++ src/main/java/Repositorio/VehiculoRepositorio.java
@@ -1,5 +1,17 @@
 package Repositorio;
 import Modelo.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface VehiculoRepositorio extends JpaRepository<Vehiculo, Long> {
+    Vehiculo save (Vehiculo vehiculo);
+    Optional<Vehiculo> findById(Long id);
+    List<Vehiculo> findAll();
+    void deleteById (Long id);
+    void delete (Vehiculo vehiculo);
+    long count();
+    List <Vehiculo> findByMatricula (String matricula);
+
 }
